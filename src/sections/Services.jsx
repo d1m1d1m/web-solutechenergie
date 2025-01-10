73a2ddd97fa0f6393e82ff3f4677428c614f013f
d1m1d1m <d1m1tr1.d3l3tang@gmail.com>
2025-01-10 20:02:41 +0100

    update service card

--- src/sections/Services.jsx
+++ src/sections/Services.jsx
@@ -5,8 +5,51 @@ const ServiceSection = () => {
 		<section id="service" className="container">
 			<h2 className="section__title">Nos services</h2>
 
-			<ul>
-				<li></li>
+			<ul role="list" className="services">
+				<li className="service">
+					<h3>Plomberie</h3>
+					<p>
+						Lorem ipsum, dolor sit amet consectetur adipisicing
+						elit. Lorem ipsum dolor, sit amet consectetur
+						adipisicing elit.
+					</p>
+					<a href="#">En savoir +</a>
+				</li>
+				<li className="service">
+					<h3>Chauffagerie</h3>
+					<p>
+						Lorem ipsum, dolor sit amet consectetur adipisicing
+						elit.
+					</p> 
+				</li>
+				<li className="service">
+					<h3>Climatisation</h3>
+					<p>
+						Lorem ipsum, dolor sit amet consectetur adipisicing
+						elit.
+					</p>
+				</li>
+				<li className="service">
+					<h3>Petits travaux</h3>
+					<p>
+						Lorem ipsum, dolor sit amet consectetur adipisicing
+						elit.
+					</p>
+				</li>
+				<li className="service">
+					<h3>Petits travaux</h3>
+					<p>
+						Lorem ipsum, dolor sit amet consectetur adipisicing
+						elit.
+					</p>
+				</li>
+				<li className="service">
+					<h3>Petits travaux</h3>
+					<p>
+						Lorem ipsum, dolor sit amet consectetur adipisicing
+						elit.
+					</p>
+				</li>
 			</ul>
 		</section>
 	);
